41e058bc5d6c349afb63c67aa87385e0dad2c55a
David Amante <amante2hfdus@gmail.com>
2022-06-01 23:00:05 +0800

    Fix: control signals bit width
    Accidentally kept the signals at 9 bits even after it was changed to 10 bits, causing regwrite to be set to X instead of 0/1.

--- maindec.sv
+++ maindec.sv
@@ -14,12 +14,12 @@ module maindec(input  logic [5:0] op,
 
   always_comb
     case(op)
-      6'b000000: controls <= 9'b1100000100; // RTYPE
-      6'b100011: controls <= 9'b1010010000; // LW
-      6'b101011: controls <= 9'b0010100000; // SW
-      6'b000100: controls <= 9'b0001000010; // BEQ
-      6'b001000: controls <= 9'b1010000000; // ADDI
-      6'b000010: controls <= 9'b0000001000; // J
-      default:   controls <= 9'bxxxxxxxxxx; // illegal op
+      6'b000000: controls <= 10'b1100000100; // RTYPE
+      6'b100011: controls <= 10'b1010010000; // LW
+      6'b101011: controls <= 10'b0010100000; // SW
+      6'b000100: controls <= 10'b0001000010; // BEQ
+      6'b001000: controls <= 10'b1010000000; // ADDI
+      6'b000010: controls <= 10'b0000001000; // J
+      default:   controls <= 10'bxxxxxxxxxx; // illegal op
     endcase
 endmodule
